b41913477d6b144a0303db13a580879a7dfe871c
fai7h01 <l.varsimashvil10@gmail.com>
2024-09-26 16:44:21 +0400

    city controller created

---
+++ src/main/java/com/localnews/controller/CityController.java
@@ -0,0 +1,48 @@
+package com.localnews.controller;
+
+import com.localnews.dto.CityDto;
+import com.localnews.dto.ResponseWrapper;
+import com.localnews.service.CityService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/city")
+public class CityController {
+
+    private final CityService cityService;
+
+    public CityController(CityService cityService) {
+        this.cityService = cityService;
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<ResponseWrapper> getAllCities(){
+        List<CityDto> allCities = cityService.getAllCities();
+        return ResponseEntity.ok(ResponseWrapper.builder()
+                .success(true)
+                .code(HttpStatus.OK.value())
+                .data(allCities).build());
+    }
+
+    @GetMapping("/{cityId}")
+    public ResponseEntity<ResponseWrapper> getCityById(@PathVariable("cityId") Long id){
+        CityDto city = cityService.findById(id);
+        return ResponseEntity.ok(ResponseWrapper.builder()
+                .success(true)
+                .code(HttpStatus.OK.value())
+                .data(city).build());
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseWrapper> addCity(@RequestBody CityDto city){
+        CityDto saved = cityService.save(city);
+        return ResponseEntity.ok(ResponseWrapper.builder()
+                .success(true)
+                .code(HttpStatus.OK.value())
+                .data(saved).build());
+    }
+}
--- src/main/java/com/localnews/controller/NewsController.java
+++ src/main/java/com/localnews/controller/NewsController.java
@@ -1,38 +1,23 @@
 package com.localnews.controller;
 
-import com.localnews.dto.CityDto;
 import com.localnews.dto.NewsDto;
 import com.localnews.dto.ResponseWrapper;
-import com.localnews.service.CityService;
 import com.localnews.service.NewsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/api/v1/news-app")
+@RequestMapping("/api/v1/news")
 public class NewsController {
 
     private final NewsService newsService;
-    private final CityService cityService;
 
-    public NewsController(NewsService newsService, CityService cityService) {
+    public NewsController(NewsService newsService) {
         this.newsService = newsService;
-        this.cityService = cityService;
     }
 
-    @GetMapping("/cities")
-    public ResponseEntity<ResponseWrapper> getAllCities(){
-        List<CityDto> allCities = cityService.getAllCities();
-        return ResponseEntity.ok(ResponseWrapper.builder()
-                .success(true)
-                .code(HttpStatus.OK.value())
-                .data(allCities).build());
-    }
-
-    @GetMapping("/articleByCity")
+    @GetMapping("/search")
     public ResponseEntity<ResponseWrapper> getContentByCity(@RequestParam("city") String city){
         NewsDto newsByCity = newsService.getNewsByCity(city);
         return ResponseEntity.ok(ResponseWrapper.builder()
